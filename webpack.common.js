741bf09acd6eb0b3a313824904655262e60840fa
Hellstaff8822 <fgjd.gh50@gmail.com>
2025-09-12 11:29:56 +0300

    test-6

--- webpack.common.js
+++ webpack.common.js
@@ -1,7 +1,39 @@
+const fs = require("fs");
 const path = require("path");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const AssetsPlugin = require("assets-webpack-plugin");
+
+// Плагін для оновлення Hugo template
+class HugoTemplatePlugin {
+  apply(compiler) {
+    compiler.hooks.afterEmit.tap("HugoTemplatePlugin", (compilation) => {
+      let cssFile = "",
+        jsFile = "";
+
+      for (const filename in compilation.assets) {
+        if (filename.startsWith("main.") && filename.endsWith(".css")) {
+          cssFile = filename;
+        }
+        if (filename.startsWith("main.") && filename.endsWith(".js")) {
+          jsFile = filename;
+        }
+      }
+
+      // Оновлюємо baseof.html
+      const templatePath = path.join(
+        __dirname,
+        "site/layouts/_default/baseof.html"
+      );
+      if (fs.existsSync(templatePath)) {
+        let template = fs.readFileSync(templatePath, "utf8");
+        template = template.replace(/\/main\.[a-f0-9]+\.css/g, `/${cssFile}`);
+        template = template.replace(/\/main\.[a-f0-9]+\.js/g, `/${jsFile}`);
+        fs.writeFileSync(templatePath, template);
+      }
+    });
+  }
+}
 
 module.exports = {
   entry: {
@@ -10,7 +42,7 @@ module.exports = {
 
   output: {
     path: path.join(__dirname, "dist"),
-    publicPath: ""
+    publicPath: "",
   },
 
   module: {
@@ -21,14 +53,14 @@ module.exports = {
         options: {
           name: "[hash].[ext]",
           outputPath: "fonts/",
-          publicPath: "../fonts/"
-        }
+          publicPath: "../fonts/",
+        },
       },
       {
         loader: "babel-loader",
         test: /\.js?$/,
         exclude: /node_modules/,
-        options: {cacheDirectory: true}
+        options: { cacheDirectory: true },
       },
       {
         test: /\.(sa|sc|c)ss$/,
@@ -38,12 +70,12 @@ module.exports = {
           {
             loader: MiniCssExtractPlugin.loader,
             options: {
-              esModule: false
-            }
+              esModule: false,
+            },
           },
           {
             loader: "css-loader",
-            options: {url: false}
+            options: { url: false },
           },
           "postcss-loader",
           {
@@ -53,26 +85,27 @@ module.exports = {
                 outputStyle: "expanded",
               },
             },
-          }
-        ]
-      }
-    ]
+          },
+        ],
+      },
+    ],
   },
 
   plugins: [
     new AssetsPlugin({
       filename: "webpack.json",
       path: path.join(process.cwd(), "site/data"),
-      prettyPrint: true
+      prettyPrint: true,
     }),
     new CopyWebpackPlugin({
       patterns: [
         // Only copy the .keep file
         {
           from: "./src/fonts/.keep",
-          to: "fonts/.keep"
-        }
-      ]
+          to: "fonts/.keep",
+        },
+      ],
     }),
-  ]
+    new HugoTemplatePlugin(),
+  ],
 };
